67b6d7a2c8049e0f4514dc73ba4e8461ac22e8db
david <davidtimote466@gmail.com>
2024-02-23 15:21:39 -0500

    Segundo plano

--- script2.sh
+++ script2.sh
@@ -4,3 +4,10 @@
 
 read -P "script2#" folder_name
 mkdir "$folder_name"
+
+#Ejecutar un comando en el segundo plano
+
+echo "Executing a command in the background"
+command_to_run="start /b notepad.exe"
+nohup $command_to_run > output.txt 2>&1 &
+echo " the command is running"
